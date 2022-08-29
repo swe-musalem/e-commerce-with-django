c00c0400456593629a261df7f1bef4a00efb344a
musalem <111184820+swe-musalem@users.noreply.github.com>
2022-08-29 05:37:54 +0300

    add namespacing & dynamuc url for product detail

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
+# namespacing for this app
+app_name = 'myapp'
 
 """root url myapp/ """
 urlpatterns = [
     path('app/',views.index),
     path('products/',views.products),
     #specify the data type and send it to the parameter in the view function
-    path('products-detail/<int:id>/', views.product_detail),
+    path('products-detail/<int:id>/', views.product_detail, name='product_detail'),
 ]
